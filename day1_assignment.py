059c28e6c0b6931512c51cda6395ae31aec0c7b1
rsriram23 <rsriram.msec@gmail.com>
2026-01-27 17:12:06 +0530

    adding day1 assignment python file

---
+++ day1_assignment.py
@@ -0,0 +1,22 @@
+# assignment 1 -from scratch(new project-venv..) to 3 open ecommerce
+from selenium import webdriver
+import time
+
+opti=webdriver.ChromeOptions()
+opti.add_experimental_option('detach',True)
+chrome_driver=webdriver.Chrome(opti)
+print('opening amazon')
+chrome_driver.get('https://amazon.in')
+time.sleep(2)
+print('title: ',chrome_driver.title)
+print('opening flipkart')
+chrome_driver.get('https://flipkart.com')
+time.sleep(2)
+print('Title now.. ',chrome_driver.title)
+print('opening myntra')
+chrome_driver.get('https://myntra.com')
+time.sleep(2)
+print('and now title-> ',chrome_driver.title)
+
+chrome_driver.close()
+# assignment 1
